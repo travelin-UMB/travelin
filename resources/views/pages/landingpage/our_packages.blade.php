ae2950611ecd47eb4b4e46be8d59eb26389cc936
fajarsubeki <fajarsub06@gmail.com>
2022-03-29 21:20:57 +0700

    initial commit

--- resources/views/pages/landingpage/our_packages.blade.php
+++ resources/views/pages/landingpage/our_packages.blade.php
@@ -27,11 +27,11 @@
 		                <div class="select-wrap one-third">
 	                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
 	                    <select name="" id="" class="form-control" placeholder="Keyword search">
-	                      <option value="">Select Location</option>
-	                      <option value="">San Francisco USA</option>
-	                      <option value="">Berlin Germany</option>
-	                      <option value="">Lodon United Kingdom</option>
-	                      <option value="">Paris Italy</option>
+	                      <option value="">Select City</option>
+	                      <option value="">Bali</option>
+	                      <option value="">Bengkulu</option>
+	                      <option value="">Lombok</option>
+	                      <option value="">Yogyakarta</option>
 	                    </select>
 	                  </div>
 		              </div>
@@ -40,17 +40,6 @@
 		              </div>
 		              <div class="form-group">
 		                <input type="text" id="checkin_date" class="form-control" placeholder="Date to">
-		              </div>
-		              <div class="form-group">
-		              	<div class="range-slider">
-		              		<span>
-										    <input type="number" value="25000" min="0" max="120000"/>	-
-										    <input type="number" value="50000" min="0" max="120000"/>
-										  </span>
-										  <input value="1000" min="0" max="120000" step="500" type="range"/>
-										  <input value="50000" min="0" max="120000" step="500" type="range"/>
-										  </svg>
-										</div>
 		              </div>
 		              <div class="form-group">
 		                <input type="submit" value="Search" class="btn btn-primary py-3 px-5">
