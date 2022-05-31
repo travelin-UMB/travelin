7c564d338245a14f2578a0d249d3608fe4f77316
daniel <daniel.yohanes@mncgroup.com>
2022-05-31 15:22:59 +0700

    fixing reservation

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\reservation;
+use App\Models\Reservation;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
